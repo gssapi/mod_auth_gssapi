09104abbab05f92bf1f489fb8e4ee5ab3c2bec1a
Isaac Boukris <iboukris@gmail.com>
2015-07-27 01:26:41 +0300

    Add test for basic auth with two different users over the same connection
    Make sure each request is authenticated according to given credentials
    even when GssapiConnectionBound is set.
    
    Reviewed-by: Simo Sorce <simo@redhat.com>

---
+++ tests/t_basic_k5_two_users.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# Copyright (C) 2015 - mod_auth_gssapi contributors, see COPYING for license.
+
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == '__main__':
+    s = requests.Session()
+
+    url = 'http://%s:%s@%s/basic_auth_krb5/' % (os.environ['MAG_USER_NAME'],
+                                                os.environ['MAG_USER_PASSWORD'],
+                                                os.environ['NSS_WRAPPER_HOSTNAME'])
+    r = s.get(url)
+    if r.status_code != 200:
+        raise ValueError('Basic Auth Failed')
+
+    url = 'http://%s:%s@%s/basic_auth_krb5/' % (os.environ['MAG_USER_NAME_2'],
+                                                os.environ['MAG_USER_PASSWORD_2'],
+                                                os.environ['NSS_WRAPPER_HOSTNAME'])
+    r2 = s.get(url)
+    if r2.status_code != 200:
+        raise ValueError('Basic Auth failed')
+
+    if r.text == r2.text:
+         raise ValueError('Basic Auth fatal error')
