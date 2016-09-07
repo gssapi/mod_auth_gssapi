0c4d113b7379ac4eea6a51f1c510881095caf9e8
Robbie Harwood <rharwood@redhat.com>
2016-09-07 15:56:57 -0400

    Add simple script for generating session keys
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>
    Reviewed-by: Simo Sorce <simo@redhat.com>
    Closes #105

---
+++ contrib/session_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Works with both python2 and python3; please preserve this property
+
+# Copyright (C) 2016 mod_auth_gssapi contributors - See COPYING for (C) terms
+
+# Simple script to generate GssapiSessionKey values
+
+import base64
+import os
+
+bits = base64.b64encode(os.urandom(32))
+print("GssapiSessionKey key:" + bits.decode('utf-8'))
