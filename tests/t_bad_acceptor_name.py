d61862b138858721d6baf06ab2ad29ebb566f09b
Simo Sorce <simo@redhat.com>
2017-03-01 13:05:59 -0500

    Add test to check when an acceptor name is bad
    Had this in my tree but forgot to add to the commit.
    Related to #131
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

---
+++ tests/t_bad_acceptor_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Copyright (C) 2015 - mod_auth_gssapi contributors, see COPYING for license.
+
+import os
+import requests
+from stat import ST_MODE
+from requests_kerberos import HTTPKerberosAuth, OPTIONAL
+
+
+if __name__ == '__main__':
+    sess = requests.Session()
+    url = 'http://%s/bad_acceptor_name/' % os.environ['NSS_WRAPPER_HOSTNAME']
+    r = sess.get(url, auth=HTTPKerberosAuth(delegate=True))
+    if r.status_code != 200:
+        raise ValueError('Bad Acceptor Name failed')
