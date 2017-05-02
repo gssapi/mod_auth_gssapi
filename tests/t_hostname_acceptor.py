a94e7c58aedf016c6e7f1fae2207ec5b1a6f948c
Simo Sorce <simo@redhat.com>
2017-05-02 09:53:54 -0400

    Allow sourcing the acceptor_name from httpd
    The hostname the client uses to connect to apache is stored in the
    request structure for modules to use.
    This patch adds an option to specify that the acceptor name to be used
    should be constructed on a per-request basis from this name.
    This is simply achieved by specifying the literal string {HOSTNAME} to
    the existing GssapiAcceptorName option insted of specifying a full
    GSSAPI name.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>
    Resolves #138

---
+++ tests/t_hostname_acceptor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Copyright (C) 2015 - mod_auth_gssapi contributors, see COPYING for license.
+
+import os
+import requests
+import sys
+from stat import ST_MODE
+from requests_kerberos import HTTPKerberosAuth, OPTIONAL
+
+
+if __name__ == '__main__':
+    sess = requests.Session()
+    url = 'http://%s/hostname_acceptor/' % sys.argv[1]
+    r = sess.get(url, auth=HTTPKerberosAuth(delegate=True))
+    if r.status_code != 200:
+        raise ValueError('Hostname-based acceptor failed')
