b2e758518947f9857eef98207b0ecd07f69b4aca
Robbie Harwood <rharwood@redhat.com>
2018-12-21 11:53:32 -0500

    Modernize Travis for new distros and python3
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- .travis.sh
+++ .travis.sh
@@ -10,27 +10,29 @@ if [ -f /etc/debian_version ]; then
     apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get -y install $COMPILER pkg-config \
                    apache2-bin {apache2,libkrb5,libssl,gss-ntlmssp}-dev \
-                   python-{dev,requests,gssapi} lib{socket,nss}-wrapper \
+                   python3-{dev,requests,gssapi} lib{socket,nss}-wrapper \
                    flex bison krb5-{kdc,admin-server,pkinit} \
                    flake8 virtualenv
     flake8
 
     # remove when python-requests-gssapi is packaged in Debian
-    virtualenv --system-site-packages .venv
+    virtualenv -p python3 --system-site-packages .venv
     source .venv/bin/activate
     pip install requests-gssapi
 elif [ -f /etc/redhat-release ]; then
     DY=yum
+    PYTHON=python2
     if [ -f /etc/fedora-release ]; then
         DY=dnf
+        PYTHON=python3
     fi
     $DY -y install $COMPILER python-gssapi krb5-{server,workstation,pkinit} \
         {httpd,krb5,openssl,gssntlmssp}-devel {socket,nss}_wrapper \
-        python-requests autoconf automake libtool which bison make \
-        flex mod_session redhat-rpm-config python2-virtualenv
+        autoconf automake libtool which bison make $PYTHON \
+        flex mod_session redhat-rpm-config /usr/bin/virtualenv
 
     # remove when we're using f28+
-    virtualenv .venv
+    virtualenv -p $PYTHON .venv
     source .venv/bin/activate
     pip install requests{,-gssapi}
     if [ x$COMPILER == xclang ]; then
