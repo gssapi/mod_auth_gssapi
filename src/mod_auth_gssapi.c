gssapi/mod_auth_gssapi

commit 9c555e2a42653153c0f0159446b90a26c14e9465

Pass GSS_C_NO_OID as the mechanism to gss_localname.
Passing the actual mechanism name does not work as expected
with SPNEGO-wrapped names.

More details in http://krbdev.mit.edu/rt/Ticket/Display.html?id=8782

Signed-off-by: Alejandro Perez <alex.perez-mendez@jisc.ac.uk>

src/mod_auth_gssapi.c
@@ -1271,7 +1271,11 @@ static int mag_complete(struct mag_req_cfg *req_cfg, struct mag_conn *mc,
 #endif
 
     if (cfg->map_to_local) {
-        maj = gss_localname(&min, client, mech_type, &lname);
+        /* We pass GSS_C_NO_OID here as passing mech_type does not work
+         * as expected with SPNEGO-wrapped names.
+         * http://krbdev.mit.edu/rt/Ticket/Display.html?id=8782
+         */
+        maj = gss_localname(&min, client, GSS_C_NO_OID, &lname);
         if (maj != GSS_S_COMPLETE) {
             mag_post_error(req, cfg, MAG_GSS_ERR, maj, min,
                            "gss_localname() failed");

commit 4872d196b5a6d926bdc1b7f54d57d7b12e58cac6

Fix gss_localname with SPNEGO wrapping
This fixes a regression introduced with commit:
9c555e2a42653153c0f0159446b90a26c14e9465

Thanks to Matt Woodyard for patient help in debugging this issue.

Signed-off-by: Simo Sorce <simo@redhat.com>

src/mod_auth_gssapi.c
@@ -1268,11 +1268,21 @@ static int mag_complete(struct mag_req_cfg *req_cfg, struct mag_conn *mc,
 #endif
 
     if (cfg->map_to_local) {
-        /* We pass GSS_C_NO_OID here as passing mech_type does not work
+        /* We have to play heuristics here as gss_localname does not work
          * as expected with SPNEGO-wrapped names.
          * http://krbdev.mit.edu/rt/Ticket/Display.html?id=8782
          */
-        maj = gss_localname(&min, client, GSS_C_NO_OID, &lname);
+        maj = gss_localname(&min, client, mech_type, &lname);
+        if (maj != GSS_S_COMPLETE) {
+            uint32_t sub_maj, sub_min;
+            /* try fallback with no oid */
+            sub_maj = gss_localname(&sub_min, client, GSS_C_NO_OID, &lname);
+            if (sub_maj != GSS_S_UNAVAILABLE) {
+                /* use second call errors only if they are meaningful */
+                maj = sub_maj;
+                min = sub_min;
+            }
+        }
         if (maj != GSS_S_COMPLETE) {
             mag_post_error(req, cfg, MAG_GSS_ERR, maj, min,
                            "gss_localname() failed");