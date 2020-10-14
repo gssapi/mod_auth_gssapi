73397865b339cc289a33dcaf6cf68bfd0cf802b8
Simo Sorce <simo@redhat.com>
2020-10-14 15:47:56 -0400

    Add test that exercises S4u2Proxy code
    This test shows that currently GssapiAcceptor {HOSTNAME} option will
    break the S4U2Proxy case.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>
    [rharwood@redhat.com: nits]

--- tests/t_hostname_acceptor.py
+++ tests/t_hostname_acceptor.py
@@ -9,7 +9,7 @@ from requests_gssapi import HTTPKerberosAuth, OPTIONAL # noqa
 
 if __name__ == '__main__':
     sess = requests.Session()
-    url = 'http://%s/hostname_acceptor/' % sys.argv[1]
+    url = 'http://{}/{}/'.format(sys.argv[1], sys.argv[2])
     r = sess.get(url, auth=HTTPKerberosAuth(delegate=True))
     if r.status_code != 200:
-        raise ValueError('Hostname-based acceptor failed')
+        raise ValueError('Hostname acceptor ({}) failed'.format(sys.argv[2]))
